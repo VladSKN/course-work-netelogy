73258f9c8f7b142f4fcb9cc9c660888017e3e59d
Vlad Semikin <vladsemikin3@gmail.com>
2021-07-27 08:19:48 +0300

    0.1

--- src/ru/netology/graphics/ru/netology/graphics/image/TextColorSchema.java
+++ src/ru/netology/graphics/ru/netology/graphics/image/TextColorSchema.java
@@ -4,30 +4,23 @@ public class TextColorSchema implements ru.netology.graphics.image.TextColorSche
 
     @Override
     public char convert(int color) {
-        if ((color >= 0)&&(color <= 32)){
+        if (color <= 32) {
             return '#';
-        }
-        if ((color >= 32)&&(color <= 64)){
+        } else if (color <= 64) {
             return '$';
-        }
-        if ((color >= 64)&&(color <= 96)){
+        } else if (color <= 96) {
             return '@';
-        }
-        if ((color >= 96)&&(color <= 128)){
+        } else if (color <= 128) {
             return '%';
-        }
-        if ((color >= 128)&&(color <= 160)){
+        } else if (color <= 160) {
             return '*';
-        }
-        if ((color >= 160)&&(color <= 192)){
+        } else if (color <= 192) {
             return '+';
-        }
-        if ((color >= 192)&&(color <= 224)){
+        } else if (color <= 224) {
             return '-';
-        }
-        if ((color >= 224)&&(color <= 255)){
+        } else if (color <= 255) {
             return '\'';
         }
-        return 0;
+        return ' ';
     }
 }
